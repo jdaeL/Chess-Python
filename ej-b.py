8d436909261cf80b070e2c4d7a070ace7a3aa5aa
jdaeL <joedaniel0804@gmail.com>
2023-05-29 12:31:14 -0500

    ejb: 2x2 con horizontalmirror

--- ej-b.py
+++ ej-b.py
@@ -1,3 +1,16 @@
 from chessPictures import *
 from interpreter import draw
+from colors import inverter
 
+white_knight = knight
+black_knight = knight.negative()
+
+# horizontalMirror() para modificar hacia donde miran los caballos
+mirror_white_knight = white_knight.horizontalMirror()
+mirror_black_knight = black_knight.horizontalMirror()
+
+row1 = white_knight.join(black_knight)
+row2 = mirror_black_knight.join(mirror_white_knight)
+board = row1.under(row2)
+
+draw(board)
