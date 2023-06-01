e02069123fce15b3ef5d7abdf1e834416bceeeff
jdaeL <joedaniel0804@gmail.com>
2023-06-01 00:47:43 -0500

    Correción en Ej-B

--- ej-b.py
+++ ej-b.py
@@ -6,8 +6,8 @@ white_knight = knight
 black_knight = knight.negative()
 
 # horizontalMirror() para modificar hacia donde miran los caballos
-mirror_white_knight = white_knight.horizontalMirror()
-mirror_black_knight = black_knight.horizontalMirror()
+mirror_white_knight = white_knight.verticalMirror()
+mirror_black_knight = black_knight.verticalMirror()
 
 row1 = white_knight.join(black_knight)
 row2 = mirror_black_knight.join(mirror_white_knight)
